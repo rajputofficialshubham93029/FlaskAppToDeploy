e75e2d69d538de8eff0c255ac49ef51ce4ee11a4
Shubham Rajput <rajputofficialshubham93029@gmail.com>
2023-03-17 08:52:12 +0530

    data base connection added

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask , render_template
+import datetime
+from flask import Flask , render_template , request
+from pymongo import MongoClient
+import urllib
+import os
 app = Flask(__name__)
+mongo_uri = "mongodb+srv://rajputsrshubham930:" + urllib.parse.quote("Shubham123@") + "@todolistappication.tn0o23o.mongodb.net/test"
+client = MongoClient(mongo_uri)
+app.db = client.todolistapplicationdata
+
+entries = []
 
 @app.route("/")
 def hello():
-    return render_template("hello.html")
+    entries = [(
+                entry['content'] , entry['date']
+
+        ) for entry in app.db.entries.find({})]
+    return render_template("hello.html"  , entries=entries)
